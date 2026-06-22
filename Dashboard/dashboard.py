b01eab9b42b644457da8e0fae54dcc6c80354fbf
Michelle Richardson <richarm4@sunypoly.edu>
2026-06-22 00:58:54 -0400

    Fix data path

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -2,28 +2,32 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import numpy as np
+from pathlib import Path
 
+
+SCRIPT_DIR = Path(__file__).resolve().parent
+
+# Build the absolute path to your Data folder
+data_path = SCRIPT_DIR / ".." / "Data"
 
 # Data import
-data_path = "../Data"
-
-pco2_or = f"{data_path}/ooi_oregon_pco2_hourly.csv"
+pco2_or = data_path / "ooi_oregon_pco2_hourly.csv"
+print(pco2_or)
 pco2_or_df = pd.read_csv(pco2_or)
 pco2_or_df['time'] = pd.to_datetime(pco2_or_df['time'])
 
-ph_or = f"{data_path}/ooi_oregon_shelf_pH.csv"
+ph_or = data_path / "ooi_oregon_shelf_pH.csv"
 ph_or_df = pd.read_csv(ph_or)
 ph_or_df.loc[ph_or_df['pH'] < 0, 'pH'] = np.nan  # Remove anomoly; apparent sensor malfunction
 ph_or_df['time'] = pd.to_datetime(ph_or_df['time'])
 
-pco2_wa = f"{data_path}/ooi_washington_pco2_hourly.csv"
+pco2_wa = data_path / "ooi_washington_pco2_hourly.csv"
 pco2_wa_df = pd.read_csv(pco2_wa)
 pco2_wa_df['time'] = pd.to_datetime(pco2_wa_df['time'])
 
-ph_wa = f"{data_path}/ooi_washington_shelf_pH.csv"
+ph_wa = data_path / "ooi_washington_shelf_pH.csv"
 ph_wa_df = pd.read_csv(ph_wa)
 ph_wa_df['time'] = pd.to_datetime(ph_wa_df['time'])
-
 
 # Streamlit and plots
 st.title("📊 Ocean Observatories Initiative (OOI) - Highlights from the Coastal Endurance Array")
