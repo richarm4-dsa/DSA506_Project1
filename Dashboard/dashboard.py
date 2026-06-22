f8cf09ad6122ba6030bedf47f7c48ddfb8ac6883
Michelle Richardson <richarm4@sunypoly.edu>
2026-06-22 01:22:58 -0400

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -33,46 +33,80 @@ ph_wa_df['time'] = pd.to_datetime(ph_wa_df['time'])
 # Streamlit and plots
 st.title("📊 Ocean Observatories Initiative (OOI) - Highlights from the Coastal Endurance Array")
 
+# Oregon
 sea_nominal_pH = 8.1
+
+# Oregon
 platform = 'Oregon Shelf Mooring'
 min_date = ph_or_df['time'].min().strftime('%m-%Y')
 max_date = ph_or_df['time'].max().strftime('%m-%Y')
-fig1 = px.line(ph_or_df,x='time',y='pH',
+fig1o = px.line(ph_or_df,x='time',y='pH',
         title=f'Seawater pH ({min_date} - {max_date}), {platform}')
-fig1.add_hline(sea_nominal_pH)
-
+fig1o.add_hline(sea_nominal_pH)
 
 min_date = ph_or_df['time'].min().strftime('%m-%Y')
 max_date = ph_or_df['time'].max().strftime('%m-%Y')
-fig2 = px.line(data_frame=pco2_or_df,x='time',y='partial_pressure_co2_ssw',
+fig2o = px.line(data_frame=pco2_or_df,x='time',y='partial_pressure_co2_ssw',
         title=f'Partial Pressure of CO2 (pCO2) ({min_date} - {max_date}), {platform}',
         labels={'partial_pressure_co2_ssw':'Pressure (microatm)'})
 
+pco2_or_df.set_index('time',inplace=True)
+ph_or_df.set_index('time',inplace=True)
 
+pco2_or_df = pco2_or_df.groupby(level=0).mean()
+ph_or_df = ph_or_df.groupby(level=0).mean()
+
+pco2_hourly = pco2_or_df.resample('h').mean()
+pH_hourly = ph_or_df.resample('h').mean()
+
+pco2_pH_or_synced = pd.merge(pco2_hourly,pH_hourly,left_index=True,right_index=True,how='outer')
+
+fig3o = px.scatter(data_frame=pco2_pH_or_synced, x='partial_pressure_co2_ssw', y='pH', 
+                   title=f'pH vs pCO2 ({min_date} - {max_date}) - {platform}')
+fig3o.add_hline(sea_nominal_pH)
+
+
+#Washington
 platform = 'Washington Shelf Mooring'
 min_date = ph_wa_df['time'].min().strftime('%m-%Y')
 max_date = ph_wa_df['time'].max().strftime('%m-%Y')
-fig3 = px.line(ph_wa_df,x='time',y='pH',
+fig1w = px.line(ph_wa_df,x='time',y='pH',
         title=f'Seawater pH ({min_date} - {max_date}), {platform}')
-fig3.add_hline(sea_nominal_pH)
+fig1w.add_hline(sea_nominal_pH)
 
 
 min_date = ph_wa_df['time'].min().strftime('%m-%Y')
 max_date = ph_wa_df['time'].max().strftime('%m-%Y')
-fig4 = px.line(data_frame=pco2_wa_df,x='time',y='partial_pressure_co2_ssw',
+fig2w = px.line(data_frame=pco2_wa_df,x='time',y='partial_pressure_co2_ssw',
         title=f'Partial Pressure of CO2 (pCO2) ({min_date} - {max_date}), {platform}',
         labels={'partial_pressure_co2_ssw':'Pressure (microatm)'})
 
+pco2_wa_df.set_index('time',inplace=True)
+ph_wa_df.set_index('time',inplace=True)
 
+pco2_wa_df = pco2_wa_df.groupby(level=0).mean()
+ph_wa_df = ph_wa_df.groupby(level=0).mean()
+
+pco2_hourly = pco2_wa_df.resample('h').mean()
+pH_hourly = ph_wa_df.resample('h').mean()
+
+pco2_pH_wa_synced = pd.merge(pco2_hourly,pH_hourly,left_index=True,right_index=True,how='outer')
+
+fig3w = px.scatter(data_frame=pco2_pH_wa_synced, x='partial_pressure_co2_ssw', y='pH', 
+                   title=f'pH vs pCO2 ({min_date} - {max_date}) - {platform}')
+fig3w.add_hline(sea_nominal_pH)
 
 # Layout - Using Tabs to Display Multiple Plots
 tab1, tab2 = st.tabs(["Oregon Shelf Mooring", "Washington Shelf Mooring"])
 
+# Oregon
 with tab1:
-    st.plotly_chart(fig1, use_container_width=True)
-    st.plotly_chart(fig2, use_container_width=True)
+    st.plotly_chart(fig1o, use_container_width=True)
+    st.plotly_chart(fig2o, use_container_width=True)
+    st.plotly_chart(fig3o, use_container_width=True)
 
-
+# Washington
 with tab2:
-    st.plotly_chart(fig3, use_container_width=True)
-    st.plotly_chart(fig4, use_container_width=True)
+    st.plotly_chart(fig1w, use_container_width=True)
+    st.plotly_chart(fig2w, use_container_width=True)
+    st.plotly_chart(fig3w, use_container_width=True)
